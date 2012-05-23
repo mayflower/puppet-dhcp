27b7a6ceef7afdcc05ef4e0b47e415569f966c01
codec <codec@fnord.cx>
2012-05-23 15:25:26 +0200

    added test coverage for pool dns

--- tests/init.pp
+++ tests/init.pp
@@ -9,7 +9,7 @@ class { 'dhcp':
   ntpservers   => ['us.pool.ntp.org'],
   interfaces   => ['eth0'],
   dnsupdatekey => "/etc/bind/keys.d/$ddnskeyname",
-  require      => Bind::Key[ $ddnskeyname ],
+  # require      => Bind::Key[ $ddnskeyname ],
   pxeserver    => '10.1.1.5',
   pxefilename  => 'pxelinux.0',
 }
@@ -19,6 +19,17 @@ dhcp::pool{ 'example.com':
   mask    => '255.255.255.0',
   range   => '10.1.1.100 10.1.1.200',
   gateway => '10.1.1.1',
+  dnsdomain => ['example.com', '1.1.10.in-addr.arpa'],
+  nameservers => ['10.1.1.10']
+}
+
+dhcp::pool { 'pool.example.com':
+  network => '10.2.1.0',
+  mask    => '255.255.255.0',
+  range   => '10.2.1.100 10.2.1.200',
+  gateway => '10.2.1.1',
+  dnsdomain => ['pool.example.com', '2.1.10.in-addr.arpa'],
+  nameservers => ['10.2.1.1']
 }
 
 dhcp::host {
