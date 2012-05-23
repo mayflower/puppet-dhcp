af38bbfa1432c5e658b27e4adfc6ecdbf910f7d0
codec <codec@fnord.cx>
2012-05-23 12:50:46 +0200

    add support for pool-specific dns zones

--- manifests/pool.pp
+++ manifests/pool.pp
@@ -2,12 +2,24 @@ define dhcp::pool (
     $network,
     $mask,
     $range,
-    $gateway
+    $gateway,
+    $dnsdomain=undef,
+    $nameservers=undef
   ) {
 
     include dhcp::params
 
     $dhcp_dir = $dhcp::params::dhcp_dir
+
+    $netnameservers = $nameservers ? {
+      undef   => $dhcp::nameservers,
+      default => $nameservers
+    }
+
+    $netdomain = $dnsdomain ? {
+      undef   => $dhcp::dnsdomain,
+      default => $dnsdomain
+    }
 
     concat::fragment {
         "dhcp_pool_${name}":
